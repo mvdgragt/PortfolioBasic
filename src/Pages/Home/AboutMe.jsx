d32cc37dba4690c0cedf6d63b9d241f0b251120c
mvdgragt <michiel@vandergragt.eu>
2023-09-27 22:22:27 +0200

    adjusted comment about me

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -22,7 +22,7 @@ function AboutMe() {
               className="icon"
             />
             <h3>Experience</h3>
-            <p>2+ years <br />Fullstack Development</p>
+            <p>10+ years <br />Fullstack Development</p>
           </div>
           <div className="details-container">
             <img
@@ -36,7 +36,7 @@ function AboutMe() {
         </div>
         <div className="text-container">
           <p>
-          I live by Pippi Longstocking's motto: 'How can I know that when I've never tried?' </p><br/><p>That's how I try to live my life. At the age of 20 I moved to Sweden to take on a new adventure and ended up staying! I went into teaching to share my passion for learning new things. After 20 years I was ready for another big step: changing career. I left teaching and went back to school to become a webdeveloper. I chose webdevelopment because of my love for technology, languages, coding, building great applications and solving problems.</p>
+          I live by Pippi Longstocking's motto: 'How can I know that when I've never tried?' </p><br/><p>That's how I try to live my life. At the age of 20 I moved to Sweden to take on a new adventure and ended up staying! I went into teaching to share my passion for learning new things. My passion for design started to grow and I began to focus on both digital as well as product design. I started freelancing more than 10 years ago, mainly focussing on WordPress. I loved how fast and user friendly it was for the developer as well as customer. I did all this next to my profesional career in teacher. But after 20 years I was ready for another big step: changing career. I left teaching and went back to school to become a webdeveloper. I chose webdevelopment because of my love for technology, languages, coding, building great applications and solving problems.</p>
         </div>
       </div>
     </div>
